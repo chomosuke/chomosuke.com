15a5f912ccafd53cca68a597681b647ba6342f87
chomosuke <a13323600@gmail.com>
2023-02-04 14:40:59 +1100

    a reasonably not ugly 404 page

--- pages/notfound/src/App.tsx
+++ pages/notfound/src/App.tsx
@@ -1,15 +1,68 @@
-function App() {
+import karasu from "./karasu.jpg";
+import Levenshtein from "levenshtein";
+import { Button } from "@mui/material";
+
+export function App() {
+  const subdomain = window.location.hostname;
+
+  const validSubdomains = [
+    "lumpime.chomosuke.com",
+    "catballchard.chomosuke.com",
+    "chomosuke.com",
+    "profile.chomosuke.com",
+  ];
+
+  validSubdomains.sort(
+    (a, b) =>
+      new Levenshtein(subdomain, a).distance -
+      new Levenshtein(subdomain, b).distance
+  );
+
   return (
-    <div>
-      <header>
+    <div
+      style={{
+        background: "#bdd6ae",
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <div
+        style={{
+          width: "50%",
+          height: "fit-content",
+          paddingBottom: 100,
+        }}
+      >
+        <h1>404</h1>
+        <h2>Subdomain not found!</h2>
         <p>
-          404 Not Found.<br/>
-          <br/>
-          This will become a better page soon.
+          The subdomain <code>{subdomain}</code> does not exist. Do you mean one
+          of the following instead?
         </p>
-      </header>
+        {validSubdomains.map((validSubdomain) => (
+          <>
+            <Button
+              variant="outlined"
+              href={`https://${validSubdomain}/`}
+              size="small"
+              fullWidth={true}
+            >
+              {validSubdomain}
+            </Button>
+            <br />
+            <br />
+          </>
+        ))}
+      </div>
+      <img
+        src={karasu}
+        alt="karasu"
+        width="150"
+        style={{ position: "absolute", bottom: 64, right: 64 }}
+      />
     </div>
   );
 }
-
-export default App;
--- pages/notfound/src/index.tsx
+++ pages/notfound/src/index.tsx
@@ -1,18 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
+import { grey } from "@mui/material/colors";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import "./index.css";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider
+      theme={createTheme({
+        palette: {
+          primary: {
+            main: grey[900],
+          },
+        },
+        typography: {
+          button: {
+            textTransform: "none",
+          },
+        },
+      })}
+    >
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
